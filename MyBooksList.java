ceb13fbac26922c1db950e276c6d8a6e4bd857c0
Heedo Han <hido0604@gmail.com>
2016-10-17 17:16:54 +0900

    두번째

---
+++ MyBooksList.java
@@ -0,0 +1,13 @@
+package com.project.yil;
+
+import android.app.Activity;
+
+/**
+ * Created by Eduardo on 2016-10-17.
+ */
+
+
+
+public class MyBooksList extends Activity {
+
+}
---
+++ SplashActivity.java
@@ -0,0 +1,7 @@
+package com.project.yil;
+
+/**
+ * Created by Eduardo on 2016-10-17.
+ */
+public class SplashActivity {
+}
